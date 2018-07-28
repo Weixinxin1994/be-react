519fbb450d4dc0f472464508a8b08d9650e2ece6
cbdyzj <cbdyzj@jianzhao.org>
2018-07-28 18:24:55 +0800

    Amend

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react'
 
+import * as DatePicker from 'antd/lib/date-picker'
+import 'antd/dist/antd.css'
+
+const DatePicker_ = DatePicker as any
+
 export class Page extends React.Component<{ title: string }, {}> {
 
     public render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
+                <DatePicker_ />
             </div>
         )
     }
